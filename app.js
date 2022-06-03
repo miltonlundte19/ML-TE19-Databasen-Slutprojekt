7b930af640657226d7f147d5b8ee8e6f07c04515
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-06-03 09:48:11 +0200

    ➖ sass middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
+
 const nunjucks = require('nunjucks');
 const session = require('express-session');
 
@@ -23,19 +23,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-    sassMiddleware({
-        src: path.join(__dirname, 'public'),
-        dest: path.join(__dirname, 'public'),
-        indentedSyntax: true, // true = .sass and false = .scss
-        sourceMap: true
-    })
-);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
     session({
-        secret: 'keyboard cat',
+        secret: 'sassMiddlewarehej',
         resave: false,
         saveUninitialized: true,
         cookie: { sameSite: true }
