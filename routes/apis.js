f73ded294996043a812b54cec5a56524fcb246de
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-06-03 09:01:32 +0200

    🚚 databese.js to utils map

--- routes/apis.js
+++ routes/apis.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../database');
+const pool = require('../utils/database');
 const bcrypt = require('bcrypt');
 
 router.post('/login', async (req, res, next) => {
